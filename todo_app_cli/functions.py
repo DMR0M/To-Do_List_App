1b9f716db81932b1df1bb37524058c8fec8c9b33
rdelamerced <rommeldm87@gmail.com>
2023-02-14 13:18:10 +0800

    implemented add button in todo-app gui

--- todo_app_cli/functions.py
+++ todo_app_cli/functions.py
@@ -2,7 +2,7 @@ import os
 # import datetime
 import time
 
-FILE_PATH = os.path.join(os.getcwd(), 'todo_app_cli', 'todos.txt')
+FILE_PATH = '../todos.txt'
 custom_dformat = time.strftime('%m-%d-%Y')
 
 
@@ -18,7 +18,7 @@ def get_todos(file_path=FILE_PATH) -> list[str]:
 
 
 # Function to write to the txt file
-def write_todos(file_path: str, todos_arg: list) -> None:
+def write_todos(todos_arg: list, file_path=FILE_PATH) -> None:
     """Update the text file by writing the list of to-do items"""
     with open(file_path, 'w') as txt_file:
         txt_file.writelines(todos_arg)
--- todo_app_gui/gui.py
+++ todo_app_gui/gui.py
@@ -3,12 +3,29 @@ import PySimpleGUI as sg
 
 
 label = sg.Text('Type in a to-do')
-input_box = sg.InputText(tooltip='Enter to-do')
+input_box = sg.InputText(tooltip='Enter to-do', key='todo')
 add_btn = sg.Button('Add')
 
 
 # GUI Title
 # Main Window
-window = sg.Window('My To-Do App', layout=[[label], [input_box, add_btn]])
-window.read()
+window = sg.Window('My To-Do App',
+                   layout=[[label], [input_box, add_btn]],
+                   font=('Helvetica', 15))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = f"{values['todo']}\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case "Show":
+            pass
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
