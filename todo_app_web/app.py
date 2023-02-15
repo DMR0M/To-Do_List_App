b6f070e4286721700f55c27deb13ac85a17ca77f
rdelamerced <rommeldm87@gmail.com>
2023-02-15 10:52:38 +0800

    updated layout

--- todo_app_web/app.py
+++ todo_app_web/app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 from todo_app_cli import functions
 
+
+def add_todo():
+    new_todo = st.session_state['new_todo']
+    print(new_todo)
+
+
 todos = functions.get_todos()
 
 st.title('TO-DO List App')
@@ -10,4 +16,5 @@ st.write('Increase your productivity')
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label='', placeholder='Add new todo...')
+st.text_input(label='', placeholder='Add new todo...',
+              on_change=add_todo, key='new_todo')
